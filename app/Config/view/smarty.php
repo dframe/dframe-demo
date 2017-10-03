a29201431cafd460ee941c4b913e9e698a5ed17b
Slawek Kaleta <slaszka@gmail.com>
2017-10-03 13:05:25 +0200

    update config smarty

--- app/Config/view/smarty.php
+++ app/Config/view/smarty.php
@@ -2,6 +2,7 @@
 return array(
     'setTemplateDir' => '../app/View/templates',            // Default './View/templates'
     'setCompileDir' => '../app/View/templates_c',           // Default './View/templates_c'
+    'addPluginsDir' => '',                                  // Default template dir ./Libs/Plugins/smarty
     'debugging'     => false,                               // Default False
-    'fileExtension' => '.html.php'                          // Default '.html.php'
+     'fileExtension' => '.html.php'                         // Default '.html.php'
     );
