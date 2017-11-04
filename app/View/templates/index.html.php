78a4b84f21cf9cc132adfeeac20ea672ea3c3924
Slawek Kaleta <slaszka@gmail.com>
2017-11-04 17:12:07 +0100

    update

--- app/View/templates/index.html.php
+++ app/View/templates/index.html.php
@@ -1,11 +1,18 @@
 {include file="header.html.php"}
 <div class="container">
 
-    <h1>Great Job! - You did it!</h1>
-    <p class="lead">This page you can edit in <small> View/templates/index.html.php</small> </p> <br>
-    
+    <h1 class="font-header">Great Job! - You did it!</h1>
+    <p class="lead">This page you can edit in <small> View/templates/index.html.php</small> </p>
+
+    <h3 class="font-header">First Step</h3>
+    <p class="lead font-slim">Edit your config.php file in folder web/ </p>
+    <p class="lead font-slim"> Set up your HTTP_HOST path for developer.</p>
+    <p class="lead font-slim"> Default is <code>define('HTTP_HOST', $_SERVER['HTTP_HOST'].'/Dframe-demo');</code> Change <b>/Dframe-demo</b> to 
+    your folder.</p>
+
     <br>
-    <h3>Links</h3>
+
+    <h4>Links</h4>
     Documentation <a href="http://dframeframework.com" target="_blank">https://dframeframework.com</a><br>
     Github <a href="https://github.com/dframe/dframe" target="_blank">https://github.com/dframe/dframe</a>
 </div> <!-- /container -->
