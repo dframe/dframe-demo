c134ede495914d3d3c9af82fd5beb36e3dda7912
Slawek Kaleta <slaszka@gmail.com>
2017-11-04 16:44:00 +0100

    add example model

---
+++ app/Model/Example.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Project Name
+ * Copyright (c) Firstname Lastname
+ *
+ * @license http://yourLicenceUrl/ (Licence Name)
+ */
+ 
+namespace Model;
+
+/**
+ * Here is a description of what this file is for.
+ *
+ * @author First Name <adres@email>
+ */
+
+class ExampleModel extends \Model\Model
+{
+
+    public function example()
+    {
+        $results = array(); //Mysql query results
+        return $this->methodResult(true, array('data' => $results));
+    }
+}
---
+++ app/Model/Model.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Project Name
+ * Copyright (c) Firstname Lastname
+ *
+ * @license http://yourLicenceUrl/ (Licence Name)
+ */
+
+namespace Model;
+
+/**
+ * This class includes methods for models.
+ *
+ * @abstract
+ */
+
+abstract class Model extends \Dframe\Model
+{
+
+}
