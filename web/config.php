fe5184f31250ee864c9e9c19c3d9880a32b95932
Dusta <dusta@users.noreply.github.com>
2018-05-13 11:39:49 +0200

    Comments

--- web/config.php
+++ web/config.php
@@ -8,33 +8,33 @@ $debugAllowed = array(
 if (isset($_SERVER['REMOTE_ADDR']) AND in_array($_SERVER['REMOTE_ADDR'], $debugAllowed)) {
     ini_set("display_errors", "on");  // Debug setings
     error_reporting(E_ALL);           // Debug setings
-    define('MOD_REWRITE', true);     // Mod rewrite (ex. task=page&action=login -> page/login )
+    define('MOD_REWRITE', true);      // Mod rewrite (ex. task=page&action=login -> page/login )
     define('setErrorLog', true);      // DB show error
 
 } else {
     ini_set("display_errors", "off"); // Debug setings
     error_reporting(E_ALL);           // Debug setings
-    define('MOD_REWRITE', true);     // Mod rewrite (ex. task=page&action=login -> page/login )
+    define('MOD_REWRITE', true);      // Mod rewrite (ex. task=page&action=login -> page/login )
     define('setErrorLog', false);     // DB show error
 
 }
 
 // Application configuration
 define('APP_DIR', dirname(__FILE__).'/../app/');
-define('CODING_STYLE', true); // Check PSR-2: Coding Style
+define('CODING_STYLE', true);    // Check PSR-2: Coding Style
 
 // Website configuration
-define('VERSION', "Dframe"); // Version aplication
-define('SALT', "YOURSALT123"); // SALT
+define('VERSION', "Dframe");     // Version aplication
+define('SALT', "YOURSALT123");   // SALT default: YOURSALT123
 
 if (isset($_SERVER['REMOTE_ADDR']) AND ($_SERVER['REMOTE_ADDR'] == '127.0.0.1' OR $_SERVER['REMOTE_ADDR'] == '::1')) {
-    define('HTTP_HOST', $_SERVER['HTTP_HOST'].'/Dframe-demo');
+    define('HTTP_HOST', $_SERVER['HTTP_HOST'].'/Dframe-demo');  // Dev
 } else {
-    define('HTTP_HOST', 'website.url');
+    define('HTTP_HOST', 'website.url'); // Production
 }
 
 // Database configuration
-define('DB_HOST', ""); // Database Host (localhost)
-define('DB_USER', ""); // Database Username
-define('DB_PASS', ""); // Database Password
-define('DB_DATABASE', ""); // Databese Name
+define('DB_HOST', "");      // Database Host (localhost)
+define('DB_USER', "");      // Database Username
+define('DB_PASS', "");      // Database Password
+define('DB_DATABASE', "");  // Databese Name
