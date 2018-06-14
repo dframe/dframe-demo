a4fc53bfb791da9177e44df741de5f7847c780c5
Sławomir Kaleta <slaszka@gmail.com>
2018-06-14 10:51:57 +0200

    emulate Apache's 'mod_rewrite'

---
+++ server.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * DframeFramework
+ * Copyright (c) Sławomir Kaleta
+ *
+ * @author Sławomir Kaleta <slaszka@gmail.com>
+ * @license https://github.com/dframe/dframe/blob/master/LICENCE (MIT)
+ */
+
+/**
+ * Usage:
+ * php -S localhost:8080 -t / server.php
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+if ($uri !== '/' and file_exists(__DIR__ . '/web' . $uri)) {
+    return false;
+}
+
+require_once __DIR__ . '/web/index.php';
