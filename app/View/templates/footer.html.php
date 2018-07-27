d6d4298bb3441376c3817e4190f4c29922db4dd3
Sławomir Kaleta <slaszka@gmail.com>
2018-07-27 18:01:56 +0200

    Resolves #7, Add footer

--- app/View/templates/footer.html.php
+++ app/View/templates/footer.html.php
@@ -1,3 +1,9 @@
+    <footer class="footer">
+        <div class="container">
+            <span class="text-muted">Created on <a href="https://dframeframework.com" target="_blank" title="Dframe - PHP framework">Dframe</a>.</span>
+        </div>
+    </footer>
+    
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>   
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>    
