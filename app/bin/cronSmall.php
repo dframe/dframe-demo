c4d5921014cda6493fc8e87cee25a601b7030f79
Sławomir Kaleta <slaszka@gmail.com>
2018-07-30 15:18:01 +0200

    Resolves #10, Add example Cron

---
+++ app/bin/cronSmall.php
@@ -0,0 +1,37 @@
+<?php
+set_time_limit(0);
+ini_set('max_execution_time', 0);
+date_default_timezone_set('Europe/Warsaw');
+
+require_once dirname(__DIR__) . '/../vendor/autoload.php';
+include_once dirname(__DIR__) . '/../web/Config.php';
+
+/**
+ * Self Aonymous Cron class
+ */
+return (new class () extends \Dframe\Cron\Task
+{
+    public function init()
+    {
+
+        $this->dirLog = dirname(__DIR__) . '/../app/View/cache/logs/cronSmall.txt';
+
+        if (file_exists($this->dirLog) and filemtime($this->dirLog) + 59 > time()) {
+            echo filemtime($this->dirLog) . "\n\r";
+            echo time() . "\n\r";
+            die('Time Limit. Max 59 request on seconds.');
+        }
+
+        $this->mailCron();
+    }
+
+    private function mailCron()
+    {
+        // Example Cron for Mail
+        // echo '#Updating mailCron' . "\n\r";
+        // $mailModel = $this->loadModel('Mail');
+        // $mailModel->sendMails();
+    }
+
+}
+)->init();
